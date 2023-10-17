5b344b00be7459ebef66f00e7342c8c6198c71e4
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-16 21:01:10 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print using puts funtion
+ * Description: Resolve task#5 Holberton
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
