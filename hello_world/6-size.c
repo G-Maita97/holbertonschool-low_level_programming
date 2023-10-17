d1065fbfd2553739894d0c1709b88c9b99a2f743
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-16 22:32:19 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	char letra;
-	int numero; 
+	int numero;
 	long int long_int;
 	long long int long_long_int;
 	float decimal;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(letra));
 	printf("Size of an int: %ld byte(s)\n", sizeof(numero));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long_int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long_long_int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(decimal));
-	return(0);
+	return (0);
 }
