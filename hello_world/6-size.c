41f5da62ca20c5cb5356f552e5929c7d05844340
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-16 22:09:16 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print using printf funtion
+ * Description: Resolve task#5 Holberton
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letra;
+	int numero; 
+	long int long_int;
+	long long int long_long_int;
+	float decimal;
+	
+	printf("Size of a char: %ld ", sizeof(letra)); printf("byte(s)\n");
+	printf("Size of an int: %ld ", sizeof(numero)); printf("byte(s)\n");
+	printf("Size of a long int: %ld ", sizeof(long_int)); printf("byte(s)\n");
+	printf("Size of a long long int: %ld ", sizeof(long_long_int)); printf("byte(s)\n");
+	printf("Size of a decimal: %ld ", sizeof(decimal)); printf("byte(s)\n");
+	return(0);
+}
