b4c55c1e3a02e7d635199f32000419ba134e8bba
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-16 20:18:25 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *Utilización de la función puts en lugar de printf.
+ * 
+ *El resultado debe retornar en 0.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
