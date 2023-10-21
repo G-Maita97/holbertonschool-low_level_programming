0690c3fbd3075cd4d9ff77839e809e2b19b3e727
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-21 12:18:53 -0700

    all

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * function (main) for start the program
+ * Description: using if else while
+ * Return = 0
+ */
+int main (void)
+{
+	char L = 'a';
+	for(L = 'a'; L <= 'z'; L++)
+	{
+		putchar(L);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
