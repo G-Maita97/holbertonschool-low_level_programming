2ce02ef4562fb465f25d5126973dd4ecb3090de9
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-21 15:12:20 -0700

    all

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numers.
+ * Description: function (main) for start the program.
+ * Return: 0
+ */
+int main(void)
+{
+	int N;
+
+	for (N = 0; N <= 9; N++)
+	{
+		putchar(N);
+	}
+
+	putchar('\n');
+	return (0);
+}
