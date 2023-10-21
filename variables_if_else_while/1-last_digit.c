98e87f9c5518bb78b47cf56f3491a46352f68c70
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-21 10:49:56 -0700

    all

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
- /**
+/**
  * main - using conditionals.
  * Description : in this case we using conditionals.
  * Return: 0
@@ -9,20 +9,22 @@
 int main(void)
 {
 	int n;
- 
+	int n2;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	n2=n % 10;
+	if (n2 > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n2);
 	}
-	else if (n == 0)
+	else if (n2 == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, n2);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n2);
 	}
 	return (0);
 }
