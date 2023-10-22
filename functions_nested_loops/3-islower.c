836e40687e598415b0348dabd5f86369239f9ef9
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-22 14:11:41 -0700

    all

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - see min and mayus.
+ * @c: The value to be used in the operation.
+ * Description: c variable declared for print.
+ * Return: 1
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	_putchar('\n');
+	return (1);
+}
+
+
