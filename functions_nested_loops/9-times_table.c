3567f90ea947a553a1eea87d0dba33f6f171c2bf
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-22 18:53:15 -0700

    all

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - print with putchar time table
+ * Description: print time table to the standard output.
+ * Return: 0
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (product <= 9)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('0' + product / 10);
+				}
+				_putchar('0' + product % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
