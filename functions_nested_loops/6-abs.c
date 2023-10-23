ce587456efeea65baf73424d9b3d5b342ba7c7fe
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-22 17:42:10 -0700

    all

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abd - return vale abs.
+ * @n: Variable
+ * Description: return vale abs to the standard output.
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
+
+
+
+
+
+
