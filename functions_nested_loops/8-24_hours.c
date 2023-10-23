c9dc90bdf89281ad91762110879764b36f239c7b
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-22 18:15:38 -0700

    all

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,28 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * jack_bauer - print with putchar hours and minutes.
+ * Description: print hours and minutes to the standard output.
+ * Return: 0
  */
-int main(void)
+void jack_bauer(void)
 {
-	jack_bauer();
-	return (0);
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			int hour_tens = hours / 10;
+			int hour_ones = hours % 10;
+			int minute_tens = minutes / 10;
+			int minute_ones = minutes % 10;
+
+			_putchar('0' + hour_tens);
+			_putchar('0' + hour_ones);
+			_putchar(':');
+			_putchar('0' + minute_tens);
+			_putchar('0' + minute_ones);
+			_putchar('\n');
+		}
+	}
 }
