7ef819b69d9f2fd6f39e8b17654a60d60666d96c
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-25 14:01:39 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - start de program for return with the solucion
+ * @argc: Variable type int.
+ * @argv: Variable type char.
+ * Description: print number the elements the argc.
+ * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	char *nombre;
 	(void) argc;
 	nombre = argv[0];
-	printf("%s\n" , nombre);
+	printf("%s\n", nombre);
 	return (0);
 }
+
+
