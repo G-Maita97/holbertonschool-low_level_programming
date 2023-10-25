fc4b2235a0e38936bd2b307783abc5eac0475c4e
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-25 14:05:19 -0700

    0-1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - start de program for return with the solucion
+ * @argc: Variable type int.
+ * @argv: Variable type char.
+ * Description: print number the elements the argc.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
