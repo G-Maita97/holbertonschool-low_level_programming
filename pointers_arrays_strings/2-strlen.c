96630bc7231ea6c8d5594ef68ad546c722f5116c
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-27 09:30:48 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen - functio for see size variable.
+* @s: pointer.
+* Description: see variable size.
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int T;
+
+	for (T = 0; s[T] != '\0'; T++)
+	{
+	}
+	return (T);
+}
+
