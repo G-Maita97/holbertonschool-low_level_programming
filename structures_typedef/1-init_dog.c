0e854b82c7cdde1cc058ca2a40c284525f2924b0
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-31 13:33:14 -0700

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,11 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog my_dog;
-
-	d = &my_dog;
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
+	}
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
 /**
  * struct dog - Short description.
  * @name: First member.
