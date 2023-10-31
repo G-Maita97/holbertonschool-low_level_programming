fcf103aa1f88532a0c6a8b0e4381cbebf9e715a7
G-Maita97 <gustavoalternativox@gmail.com>
2023-10-31 09:15:48 -0700

    0-main.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Short description.
+ * @name: First member.
+ * @owner: Second member.
+ * @age: Third member.
+ * Description: make dog.h for the struct dog.
+ */
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
-
-#endif 
+#endif
