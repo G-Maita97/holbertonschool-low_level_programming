686f16119d3fb20e88a71a70dd3d8956fc8067f9
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-05 12:58:03 -0800

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function actual
+ * @f: ponit to function void.
+ * @name: point variable to char
+ * Description: print name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
