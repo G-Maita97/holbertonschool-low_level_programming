02fcb656c6bcfdf5d1dcc172396e461503af8963
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-05 15:23:55 -0800

    3-main.c

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,20 @@
+#ifndef _3_CALC_H
+#define _3_CALC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+int (*get_op_func(char *s))(int, int);
+
+#endif /* _3_CALC_H */
