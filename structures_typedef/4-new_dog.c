9ebee7feeb98aae16e26a71be5a56bc7294a50df
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-05 10:31:23 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,15 +14,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
+	dog_t *new_dog;
 
-	my_dog = malloc(sizeof(dog_t));
-	if (my_dog == NULL)
+	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
-	return (my_dog);
+	new_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+
+	return (new_dog);
 }
